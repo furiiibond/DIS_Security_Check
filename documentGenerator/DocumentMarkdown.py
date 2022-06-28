60baa6edcb470ddc0a78d19fffae140926f441eb
jeanc <jeancamlap@gmail.com>
2022-06-28 16:05:06 +0200

    filename

--- documentGenerator/DocumentMarkdown.py
+++ documentGenerator/DocumentMarkdown.py
@@ -1,14 +1,14 @@
 from documentGenerator.markdowngenerator import MarkdownGenerator
 
 class DocumentMarkdown:
-    def __int__(self, documentName):
+    def __init__(self, filename):
         with MarkdownGenerator(
                 # By setting enable_write as False, content of the file is written
                 # into buffer at first, instead of writing directly into the file
                 # This enables for example the generation of table of contents
-                filename=documentName+".md", enable_write=False
+                filename=filename+".md", enable_write=False
         ) as self.doc:
-            self.doc.addHeader(1, documentName)
+            self.doc.addHeader(1, filename)
 
     def addCode(self, title, code):
         self.doc.addHeader(2, title)
